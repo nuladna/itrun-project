98e4425193b181e9212a78eb414a005ab6800613
nuladna <nuladna6@gmail.com>
2023-04-02 13:54:02 +0600

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 4,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+
+
+
+
+    });
+});
